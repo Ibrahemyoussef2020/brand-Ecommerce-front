14ce2e0e09097280201e2cec90fb97d8fca6f37e
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-06-09 12:36:27 +0400

    change some setteings in font awesome visiblity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
-
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import {config} from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 import type { Metadata } from "next";
 import ".././styles/globals.scss";
